8bdfbfc300709de91a2db71ccc40fe93c9c3b0fd
Laura Unaeze <l.unaeze5@gmail.com>
2018-09-19 19:18:57 -0700

    Input bar clears when form submits.

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Input extends Component {
+
   render() {
     return (
       <form onSubmit={this.props.handleSubmit}>
-        <input type="text" onChange={this.props.handleChange} />
+        {/* Remember to pass value as a prop to the component Input 
+            so it actually clears when you submit
+        */}
+        <input type="text" value={this.props.value} onChange={this.props.handleChange} />
         <button>Add</button>
       </form>
     );
--- src/ToDo.js
+++ src/ToDo.js
@@ -13,13 +13,12 @@ class ToDo extends Component {
   }
 
   handleSubmit = (event) => {
+    event.preventDefault();
     this.setState((prevState) => {
-      return {itemToDo: prevState.itemToDo.concat(this.state.value)}
+      return {value: '', itemToDo: prevState.itemToDo.concat(this.state.value)}
       // callback to make console.log run right after setState with
       // new data in itemToDo
-    }, () => console.log(this.state.itemToDo));
-
-    event.preventDefault();
+    }, () => console.log('item: ', this.state.itemToDo, "value: ", this.state.value));
   }
 
   handleClear = (event) => {
@@ -31,7 +30,9 @@ class ToDo extends Component {
       <div>
         <h1>Silly ToDo App</h1>
         <p>Enter an Item To Do Here:</p>
-        {/* TODO: Clear the input field after clicking submit */}
+        {/* Remember to pass value as a prop to the component Input 
+            so it actually clears when you submit
+        */}
         <Input 
           value={this.state.value} 
           handleChange={this.handleChange} 
